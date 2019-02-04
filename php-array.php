35c655c3c78371d8b6d9c1ad71a474aa5f503d62
Falconguy <mr.digital.matt@gmail.com>
2019-02-04 14:54:09 +0800

    looping through array

--- php-array.php
+++ php-array.php
@@ -3,7 +3,14 @@
 <body>
 
 <?php
-echo "My first PHP scrsssipt!";
+    
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+    foreach($age as $x => $x_value) {
+        echo "Key=" . $x . ", Value=" . $x_value;
+        echo "<br>";
+    }
+    
 ?>
 
 </body>
