bca43bf1c1c699f53e802218be42c87de412c5cd
Falconguy <mr.digital.matt@gmail.com>
2019-02-04 15:03:58 +0800

    finalized code

--- php-array.php
+++ php-array.php
@@ -2,16 +2,16 @@
 <html>
 <body>
 
-<?php
-    
-    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+    <?php
 
-    foreach($age as $p => $p_value) {
-        echo $p . " is " . $p_value . " years old.";
-        echo "<br>";
-    }
-    
-?>
+        $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+        foreach($age as $p => $p_value) {
+            echo $p . " is " . $p_value . " years old.";
+            echo "<br>";
+        }
+
+    ?>
 
 </body>
 </html>
